9541bc392caae2306a7f08cf826d49a229440908
gonzaroman <gonzaloromanmarquez@gmail.com>
2025-04-26 19:03:34 +0200

    muestra salas en home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { SalaService } from '../../services/salas.service';
+
 
 @Component({
   selector: 'app-home',
@@ -6,6 +10,31 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  salas: any[] = [];
 
+  constructor(private salaService: SalaService, private router: Router) {}
+
+  ngOnInit() {
+    this.cargarSalas();
+  }
+
+  cargarSalas() {
+    this.salaService.obtenerSalas().subscribe({
+      next: (salas) => {
+        this.salas = salas;
+      },
+      error: (err) => {
+        console.error('Error cargando salas', err);
+      }
+    });
+  }
+
+  entrarSala(id: string) {
+    this.router.navigate(['/sala', id]);
+  }
+
+  crearSala() {
+    this.router.navigate(['/crear-sala']);
+  }
 }
--- src/app/services/salas.service.ts
+++ src/app/services/salas.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SalasService {
+export class SalaService {
+  private apiUrl = '/api/salas';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  obtenerSalas(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  crearSala(nombre: string, creador: string): Observable<any> {
+    return this.http.post<any>(this.apiUrl, { nombre, creador });
+  }
 }
