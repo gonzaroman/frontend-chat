013be5259f43cf7db140b72a45e850aaf5c8cb6a
gonzaroman <gonzaloromanmarquez@gmail.com>
2025-05-18 22:27:02 +0200

    lista usuarios conectados

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
 import { Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
   private socket: Socket;
+  private platformId = inject(PLATFORM_ID);
 
   constructor() {
     this.socket = io('http://localhost:3000'); // Ajusta si tu backend corre en otra URL/puerto
+
+    if (isPlatformBrowser(this.platformId)) {
+      const usuario = localStorage.getItem('usuario');
+     if (usuario) {
+        this.socket.emit('usuario conectado', usuario);
+      }
+    }
   }
 
   emit(evento: string, data?: any) {
