55ee4fe72dc4bdec6ae8b0770771218e6cc0c97f
gonzaroman <gonzaloromanmarquez@gmail.com>
2025-05-18 18:55:08 +0200

    menu superior

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,36 @@
+// src/app/components/nav-bar/nav-bar.component.ts
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+  private router = inject(Router);
+  private auth   = inject(AuthService);
+   private platformId = inject(PLATFORM_ID);
+
+  usuario: string | null = null;
+
+ngOnInit() {
+    // Solo en navegador
+    if (isPlatformBrowser(this.platformId)) {
+      this.usuario = localStorage.getItem('usuario');
+    }
+  }
+ 
+
+  logout() {
+    this.auth.logout();             // Limpia localStorage
+    this.router.navigate(['/login']);
+  }
+}
+
+
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,8 +10,6 @@ export class AuthService {
   //private apiUrl = 'http://localhost:3000'; // URL del backend
   private apiUrl = '/api'; // Usamos el proxy
 
-
-
   constructor(private http: HttpClient) { }
 
   registrar(nombre: string, contraseña: string): Observable<any> {
@@ -20,6 +18,11 @@ export class AuthService {
 
   login(nombre: string, contraseña: string): Observable<any> {
     return this.http.post('/api/login', { nombre, contraseña });
+  }
+
+
+  logout(): void {
+    localStorage.removeItem('usuario');
   }
   
 }
